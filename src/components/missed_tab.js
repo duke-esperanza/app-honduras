569e48572e8893b619e7b804499fdbcdb78c7586
Dorian Barber <dpb20@duke.edu>
2018-11-28 18:13:30 -0500

    Deleted imports that are not used

--- src/components/missed_tab.js
+++ src/components/missed_tab.js
@@ -1,82 +1,88 @@
-import React, { Component } from 'react';
-import Columns from 'react-columns';
-import  style from '../styles/tabs.css';
-//import { Panel } from 'react-bootstrap';
-import Patient from './patient_list_item';
-import { Paper, Grid } from '@material-ui/core'
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import React, { Component } from "react";
+import style from "../styles/tabs.css";
+import { Paper } from "@material-ui/core";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import Typography from "@material-ui/core/Typography";
 
-class MissedTab extends Component{
-
-  constructor(props){
+class MissedTab extends Component {
+  constructor(props) {
     super(props);
 
     this.state = {
       dates: [],
       appointments: []
-    }
+    };
   }
 
   componentWillMount() {
-    var Airtable = require('airtable');
-    var base = new Airtable({apiKey: "keybQixDhiGoMKCTi"}).base('appuPqYIxCcvESuzm');
+    var Airtable = require("airtable");
+    var base = new Airtable({ apiKey: "keybQixDhiGoMKCTi" }).base(
+      "appuPqYIxCcvESuzm"
+    );
     var appointmentData = [];
-    base('Appointments').select({
-      filterByFormula: 'AND(status = 4, IS_BEFORE(date, TODAY()))',
-    }).eachPage(function page(records, fetchNextPage) {
-        records.forEach(function(record) {
-          appointmentData.push(record.fields);
-        });
-        fetchNextPage();
-    }, function done(err) {
-      if (err) { console.error(err); return; }
-      this.setState({
-        appointments: appointmentData
+    base("Appointments")
+      .select({
+        filterByFormula: "AND(status = 4, IS_BEFORE(date, TODAY()))"
       })
-    }.bind(this));
+      .eachPage(
+        function page(records, fetchNextPage) {
+          records.forEach(function(record) {
+            appointmentData.push(record.fields);
+          });
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          this.setState({
+            appointments: appointmentData
+          });
+        }.bind(this)
+      );
   }
 
   componentNameMap = (list, nameList) => {
     var map = {};
-    for(var i = 0; i < list.length; i++){
+    for (var i = 0; i < list.length; i++) {
       var name = list[i].first_name;
-      if(!map[name]){
+      if (!map[name]) {
         map[name] = [];
         nameList.push(name);
       }
       map[name].push(list[i]);
     }
     return map;
-  }
+  };
 
-  render(){
+  render() {
     var nameList = [];
     var dict = this.componentNameMap(this.state.appointments, nameList);
-    return(
+    return (
       <div>
-        <Paper className='title' elevation={2}>
-          <Typography className='title' variant='h4'>
-            <strong>
-              Missed Appointments
-            </strong>
+        <Paper className="title" elevation={2}>
+          <Typography className="title" variant="h4">
+            <strong>Missed Appointments</strong>
           </Typography>
         </Paper>
-        <Paper className='tab' elevation= {3}>
+        <Paper className="tab" elevation={3}>
           <List subheader={<li />}>
             {nameList.map(name => (
-              <li key={name} className='listSection'>
-                <ListSubheader><Typography variant='h6'>{name}</Typography></ListSubheader>
+              <li key={name} className="listSection">
+                <ListSubheader>
+                  <Typography variant="h6">{name}</Typography>
+                </ListSubheader>
                 {dict[name].map(appt => (
                   <ListItem key={appt.date}>
-                    <ListItemText primary=<Typography variant='h5'>Date: {appt.date}</Typography> />
+                    <ListItemText
+                      primary=<Typography variant="h5">
+                        Date: {appt.date}
+                      </Typography>
+                    />
                   </ListItem>
                 ))}
               </li>
@@ -84,8 +90,7 @@ class MissedTab extends Component{
           </List>
         </Paper>
       </div>
-
     );
-  };
-};
+  }
+}
 export default MissedTab;
--- src/components/today_tab.js
+++ src/components/today_tab.js
@@ -1,68 +1,73 @@
-import React, { Component } from 'react';
-import Columns from 'react-columns';
-import  style from '../styles/tabs.css';
+import React, { Component } from "react";
+import style from "../styles/tabs.css";
 //import { Panel } from 'react-bootstrap';
-import Patient from './patient_list_item';
-import TodayAppointment from './todayAppointment';
-import { Paper, Typography, Grid, List, ListItem, ListItemText } from '@material-ui/core';
-import factory from '../backend/appointmentFactory';
+import TodayAppointment from "./todayAppointment";
+import { Paper, Typography } from "@material-ui/core";
 
-
-
-class TodayTab extends Component{
-  constructor(props){
+class TodayTab extends Component {
+  constructor(props) {
     super(props);
 
     this.state = {
       title: props.title,
-      appointments: [],
-    }
+      appointments: []
+    };
   }
 
   componentWillMount() {
-    var Airtable = require('airtable');
-    var base = new Airtable({apiKey: "keybQixDhiGoMKCTi"}).base('appuPqYIxCcvESuzm');
+    var Airtable = require("airtable");
+    var base = new Airtable({ apiKey: "keybQixDhiGoMKCTi" }).base(
+      "appuPqYIxCcvESuzm"
+    );
     var appointmentData = [];
     var count = 0;
-    base('Appointments').select({
-      filterByFormula: 'IS_SAME(TODAY(), date, "day")'
-    }).eachPage(function page(records, fetchNextPage) {
-        records.forEach(function(record) {
-          appointmentData[count] = record.fields;
-          count++;
-        });
-        fetchNextPage();
-    }, function done(err) {
-      if (err) { console.error(err); return; }
-      this.setState({
-        appointments: appointmentData
+    base("Appointments")
+      .select({
+        filterByFormula: 'IS_SAME(TODAY(), date, "day")'
       })
-    }.bind(this));
+      .eachPage(
+        function page(records, fetchNextPage) {
+          records.forEach(function(record) {
+            appointmentData[count] = record.fields;
+            count++;
+          });
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          this.setState({
+            appointments: appointmentData
+          });
+        }.bind(this)
+      );
   }
 
-  render(){
-    const content = this.state.appointments.map((someData) =>
+  render() {
+    const content = this.state.appointments.map(someData => (
       <div key={someData.ID}>
-        <TodayAppointment first={someData.first_name} last={someData.last_name} time={someData.appointment_time} phone={someData.phone}/>
+        <TodayAppointment
+          first={someData.first_name}
+          last={someData.last_name}
+          time={someData.appointment_time}
+          phone={someData.phone}
+        />
       </div>
-    )
-    return(
+    ));
+    return (
       <div>
-        <Paper className='title' elevation={2}>
-          <Typography className='title' variant='h4'>
-            <strong>
-              {this.state.title}
-            </strong>
+        <Paper className="title" elevation={2}>
+          <Typography className="title" variant="h4">
+            <strong>{this.state.title}</strong>
           </Typography>
         </Paper>
-        <Paper className='tab' elevation={3}>
-          <div>
-            {content}
-          </div>
+        <Paper className="tab" elevation={3}>
+          <div>{content}</div>
         </Paper>
       </div>
-
     );
-  };
-};
+  }
+}
 export default TodayTab;
