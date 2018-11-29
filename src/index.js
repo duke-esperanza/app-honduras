b021b0424393a17df3c2db92dfc5516a6e05c7f4
tevin.brown@duke.edu <tmb51@duke.edu>
2018-11-29 11:37:40 -0500

    Created User Authentication

--- src/index.js
+++ src/index.js
@@ -3,17 +3,65 @@ import ReactDOM from 'react-dom';
 import TopBar from './components/TopBar';
 import Body from './components/body';
 import {HashRouter} from 'react-router-dom';
+import { TextField } from '@material-ui/core';
 
 
 
 
 class App extends Component{
+  state = {
+    password: localStorage.getItem('password' )||'',
+    correct:false,
+  }
+
+  componentWillMount(){
+    if(process.env.REACT_APP_PASSWORD === this.state.password){
+      this.setState({correct:true})
+    }
+  }
+
+  handleChange = () =>{
+    this.setState({password:document.getElementById('password').value})    
+  }
+
+  keyPress = (e) =>{
+    if (e.keyCode === 13){
+      if (process.env.REACT_APP_PASSWORD === this.state.password){
+        localStorage.setItem( 'password', this.state.password);
+        this.setState({correct:true})
+      }
+      else{
+        alert('Password Incorrect')
+      }
+    }
+  }
+
   render(){
       return(
-        <div>
+        <>
+          { this.state.correct ?  
+            <div>
           <TopBar/>
           <Body/>
-        </div>
+          </div>
+          :
+          <div>
+            <TextField
+            id="password"
+            type="password"
+            label="Enter the Password:"
+            defaultValue="*****"
+            fullWidth="true"
+            InputLabelProps={{
+                shrink:true,
+            }}
+            onKeyDown={this.keyPress.bind(this)}
+            onChange={this.handleChange.bind(this)}>
+
+            </TextField>
+          </div>
+          }
+        </>
       );
   }
 }
